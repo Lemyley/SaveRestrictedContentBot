2fd74578cf5f9947fda5440b729993b181e6db3d
Lemyley <jw068783@gmail.com>
2024-12-29 18:42:50 +0300

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,11 +12,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
+API_ID = config("21887008", default=None, cast=int)
+API_HASH = config("2d394dee98f44c1038d17ab7fdabe92f", default=None)
+BOT_TOKEN = config("7639728457:AAFj1Wm_ASF7MA01aDWJZNEQwBgj1ygXGGM", default=None)
+SESSION = config("BAFN-CAAuyjFHdmoNousc5BUX27tn5idcDKE2RBf-_exjcMqA_lQmIUsBTBEAOrka3z7vP78ZVlnolscFEgyeJl91rdZri27pV3o_KewJWZj8GpjyFzMC0txymyMKR4mK6k3BaP9cp-1Eg9T_H-BOozI3wsEcu1jc3Vf7rZyd2t_sJdCdwVXvAyO_UGW11NjA0HkzkdYJGd5p7pHlxBRReCpBQqgijQQJdsGXapa8AqY1he4X-zoKD3iD4wlJJzyWAm2jivxA2gWwQIoBt5yCKG4V3F-ymP409nNs2JRay9JzGK06p19q5bfjAoZK8hn4ylpEOMLkdPTyA7cPWusW4rfqdXx2QAAAAGOedYsAA", default=None)
+FORCESUB = config("@freetiktok_views", default=None)
 AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
